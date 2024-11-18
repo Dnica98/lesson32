5fd64a0569b5ed74f0a04b45dffe7208c49f3c9e
Dnica98 <daniela.nica98@gmail.com>
2024-11-18 21:42:56 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,36 +1,67 @@
 import { useState } from "react";
-import { ListItem, ToDoForm } from "./components";
+import { ListItem, Modal,ToDoForm } from "./components";
 
 
 function App() {
   const [toDo, setToDo] = useState('')
   const [toDoList, setToDoList] = useState([])
+  const [editToDo, setEditToDo] = useState('')
+  const [newToDo, setNewToDo] = useState('')
 
-  const handleChangeToDo = (value) => {  
-    setToDo(value)
+const handleChangeListToDo = () => {
+    if (!toDo) return alert('To do should not be empty')
+    if (toDoList.some(item => item.title === toDo)) return alert('To do title should be uniq')
+
+    setToDoList([...toDoList, { title: toDo, done: false }])
+    setToDo('')
   }
+  const handleEditToDo = () => {
+    if (!newToDo) return alert('To do should not be empty')
+    if (toDoList.some(item => item.title === newToDo)) return alert('To do title should be uniq')
 
-  const handleChangeListToDo = () => {
-    setToDoList([...toDoList, {title: toDo, done: false}])
-    handleChangeToDo('')
-  }  
+    setToDoList((list) => {
+      return list.map((item) => {
+        return { ...item, title: item.title === editToDo ? newToDo : item.title }
+      })
+    })
+    setEditToDo('')
+    setNewToDo('')
+  }
 
   return (
     <div>
       <ToDoForm
         title="Add to do"
         value={toDo}
-        onChange={handleChangeToDo}
+        onChange={setToDo}
         btnTitle="Add to Do"
         btnAction={handleChangeListToDo}
       />
       <ul>
         {toDoList.map((listItem) => {
-          return <ListItem key={listItem.title} title={listItem.title} done={listItem.done} />
+          return <ListItem
+            key={listItem.title}
+            title={listItem.title}
+            done={listItem.done}
+            editAction={() => {
+              setEditToDo(listItem.title)
+              setNewToDo(listItem.title)
+            }}
+          />
         })}
-      </ul>      
+      </ul>
+
+      <Modal isOpen={editToDo} closeModal={setEditToDo}>
+        <ToDoForm
+          title="Edit to do"
+          value={newToDo}
+          onChange={setNewToDo}
+          btnTitle="Edit to Do"
+          btnAction={handleEditToDo}
+          placeholder="New title"
+        />
+      </Modal>
     </div>
   );
 }
-
 export default App;
--- src/components/ListItem/ListItem.jsx
+++ src/components/ListItem/ListItem.jsx
@@ -1,14 +1,18 @@
-import EditIconScr from '../../assets/file-edit.png';
-import Icon from '../Icon';
+import EditIconSrc from '../../components/assets/file-edit.png';
+import DeleteIconSrc from '../../components/assets/delete-icon.png';
+import { Icon } from '../Icon';
 import './styles.css';
 
-const ListItem = ({ title, done }) => {
+const ListItem = ({ title, done, editAction }) => {
     return (
         <li
             className={done ? 'listItem' : ''}
         >
             {title}
-            <Icon src={EditIconScr} />
+            <div onClick={editAction}>
+                <Icon src={EditIconSrc} />
+            </div>
+            <Icon src={DeleteIconSrc} />
         </li>
     )
 }
